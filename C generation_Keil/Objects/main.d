8b12f927b380a257e4616bcf3be46ccb3cce0e32
MariamGhaly1 <mari.ats@outlook.com>
2020-06-14 01:35:46 +0200

    helper files

---
+++ C generation_Keil/Objects/main.d
@@ -0,0 +1,10 @@
+.\objects\main.o: main.c
+.\objects\main.o: E:\3th computer & systems(drive &channel)\projects\control\New folder (4)\System_20200611_0233_18_ert_rtw\System_20200611_0233_18.c
+.\objects\main.o: E:\3th computer & systems(drive &channel)\projects\control\New folder (4)\System_20200611_0233_18_ert_rtw\System_20200611_0233_18.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\objects\main.o: E:\3th computer & systems(drive &channel)\projects\control\New folder (4)\System_20200611_0233_18_ert_rtw\rtwtypes.h
+.\objects\main.o: E:\3th computer & systems(drive &channel)\projects\control\New folder (4)\System_20200611_0233_18_ert_rtw\System_20200611_0233_18_types.h
+.\objects\main.o: E:\3th computer & systems(drive &channel)\projects\control\New folder (4)\System_20200611_0233_18_ert_rtw\System_20200611_0233_18_private.h
+.\objects\main.o: E:\3th computer & systems(drive &channel)\projects\control\New folder (4)\System_20200611_0233_18_ert_rtw\ert_main.c
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
