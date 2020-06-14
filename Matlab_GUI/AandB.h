c4f50814ebee8a18fde50e3c23ef118b60ac5a75
MariamGhaly1 <41520000+MariamGhaly1@users.noreply.github.com>
2020-06-14 03:44:18 +0200

    Add files via upload

---
+++ Matlab_GUI/AandB.h
@@ -0,0 +1,4 @@
+#ifndef _AandB_H_
+#define _AandB_H_
+extern const float A[3.000000], B[3.000000];
+#endif
---
+++ Matlab_GUI/RST.h
@@ -0,0 +1,4 @@
+#ifndef _RST_H_
+#define _RST_H_
+extern const float R[3], S[51], T[51];
+#endif
